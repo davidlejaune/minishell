697d62bd2d8d8f28ffb9b7afba07079a6038b75a
David LY <dly@e1r4p13.clusters.42paris.fr>
2023-02-05 18:37:00 +0100

    save

---
+++ includes/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dly <dly@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/03 08:57:17 by mirsella          #+#    #+#             */
+/*   Updated: 2023/02/05 18:24:13 by dly              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/* readline */
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
